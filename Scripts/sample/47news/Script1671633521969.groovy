5fd139ccfdacdb631d092c48337d7edb3c6ec254
kazurayam <kazuaki.urayama@gmail.com>
2023-12-06 19:43:33 +0900

    removed unnecessary files

--- Scripts/sample/47news/Script1671633521969.groovy
+++
@@ -1,5 +0,0 @@
-import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-
-WebUI.openBrowser('https://47news.jp/')
-WebUI.delay(3)   // stay still for 3 seconds
-WebUI.closeBrowser()
